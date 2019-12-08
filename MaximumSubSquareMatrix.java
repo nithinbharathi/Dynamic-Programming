3c03c5496689d506cfc42f2aacdd0b1433a79df4
Nithin Bharathi <39995394+nithinbharathi@users.noreply.github.com>
2019-12-08 11:41:31 +0530

    Update MaximumSubSquareMatrix.java

--- MaximumSubSquareMatrix.java
+++ MaximumSubSquareMatrix.java
@@ -10,17 +10,19 @@ class MaximumSquare
 	{
 	    int arr[][] = {{0,1,1,0,1},{1,1,1,0,0},{1,1,1,1,0},{1,1,1,0,1}};
 	    int dp[][] = new int[arr.length][arr[0].length];
+		int max =0;
 	    for(int i=0;i<arr.length;i++){
 	        if(arr[i][0] == 1){
 	            dp[i][0] = 1;
+			max =1;
 	        }
 	    }
 	    for(int i=0;i<arr[0].length;i++){
 	        if(arr[0][i] == 1){
 	            dp[0][i] = 1;
+			max =1;
 	        }
 	    }
-	    int max =0;
 	    for(int i=1;i<arr.length;i++){
 	        for(int j =1;j<arr[0].length;j++){
 	            if(arr[i][j] == 1){
